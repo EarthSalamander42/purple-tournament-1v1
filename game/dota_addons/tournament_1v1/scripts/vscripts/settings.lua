5d01145187ee84a76c58828cda58c8bcf27ddded
DESKTOP-HFL3SLE\jeizy <jeizyy30@hotmail.fr>
2018-08-03 19:02:10 +0200

    * lobby setup unlocked

--- game/dota_addons/tournament_1v1/scripts/vscripts/settings.lua
+++ game/dota_addons/tournament_1v1/scripts/vscripts/settings.lua
@@ -76,7 +76,7 @@ DISABLE_STICKY_ITEM = false             -- Should we disable the sticky item but
 SKIP_TEAM_SETUP = false                 -- Should we skip the team setup entirely?
 ENABLE_AUTO_LAUNCH = true               -- Should we automatically have the game complete team setup after AUTO_LAUNCH_DELAY seconds?
 AUTO_LAUNCH_DELAY = 15					-- How long should the default team selection launch timer be?  The default for custom games is 30.  Setting to 0 will skip team selection.
-LOCK_TEAM_SETUP = true					-- Should we lock the teams initially?  Note that the host can still unlock the teams 
+LOCK_TEAM_SETUP = false					-- Should we lock the teams initially?  Note that the host can still unlock the teams 
 
 
 -- NOTE: You always need at least 2 non-bounty type runes to be able to spawn or your game will crash!
